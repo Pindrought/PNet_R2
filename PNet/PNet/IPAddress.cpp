2582bd356ad251909e904ad6da0f46b0cd19cdba
Jacob Preston <pindrought@gmail.com>
2019-02-21 10:56:15 -0500

    commit

--- PNet/PNet/IPAddress.cpp
+++ PNet/PNet/IPAddress.cpp
@@ -2,29 +2,57 @@
 
 namespace PNet
 {
-	IPAddress::IPAddress(std::string ip, unsigned short port, IPVersion ipversion)
-		:ip(ip), port(htons(port)), ipversion(ipversion)
+	IPAddress::IPAddress(std::string hostname, unsigned short port, IPVersion ipversion)
+		:hostname(hostname), port(htons(port)), ipversion(ipversion)
 	{
+		ip_string = "";
 		//Need to generate 32 bit integer representation of ip
-		address = INADDR_NONE;
-		inet_pton(AF_INET, ip.c_str(), &address); //Attempt to convert ip from presentation to network format (string to 32 bit big endian integer)
+		ip_int = INADDR_NONE;
+		inet_pton(AF_INET, hostname.c_str(), &ip_int); //Attempt to convert ip from presentation to network format (string to 32 bit big endian integer)
 
-		if (address != INADDR_NONE)
+		if (ip_int != INADDR_NONE)
 		{
+			ip_string = hostname; //If address was a valid ipv4 and not a hostname then ip_string = hostname
 			isValid = true;
 		}
 		else //
 		{
-
+			// Not a valid address, try to resolve it as a host name
+			addrinfo hints = {};
+			hints.ai_family = AF_INET;
+			addrinfo* result = NULL;
+			if (getaddrinfo(hostname.c_str(), NULL, &hints, &result) == 0)
+			{
+				if (result)
+				{
+					ip_int = reinterpret_cast<sockaddr_in*>(result->ai_addr)->sin_addr.s_addr;
+					ip_string.resize(16); //Resize ip to store ipv4 address
+					inet_ntop(AF_INET, &ip_int, &ip_string[0], 16); //Determine ip string from the integer representation of IPV4
+					freeaddrinfo(result); //memory cleanup from getaddrinfo call
+					isValid = true;
+				}
+				else
+				{
+					return; //Not valid
+				}
+			}
+			else
+			{
+				return; //Not valid
+			}
 		}
 	}
-	std::string IPAddress::ToString()
+	std::string IPAddress::GetHostname()
 	{
-		return ip;
+		return hostname;
 	}
-	uint32_t IPAddress::GetAddress()
+	std::string IPAddress::GetIPAsString()
 	{
-		return address;
+		return ip_string;
+	}
+	uint32_t IPAddress::GetIPAsInt()
+	{
+		return ip_int;
 	}
 	unsigned short IPAddress::GetPort()
 	{
